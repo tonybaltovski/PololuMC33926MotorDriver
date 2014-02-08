36954e7bd0a6d5cea465da9672f818983a71c6c6
Tony Baltovski <tony.baltovski@gmail.com>
2014-02-08 14:41:42 -0500

    Updated to work with all again by onyl defining the PWM frequeny for Teensy 3.X.

--- PololuMC33926.cpp
+++ PololuMC33926.cpp
@@ -1,51 +1,52 @@
-#include "POLOLUMC33926.h"
+#include "PololuMC33926.h"
 
 
 // Constructor
 MC33926::MC33926(int DIR1,int DIR2, int PWM, int SF)
 {
-DIR1_ = DIR1;
-DIR2_ = DIR2;
-PWM_ = PWM;
-SF_ = SF;
+	DIR1_ = DIR1;
+	DIR2_ = DIR2;
+	PWM_ = PWM;
+	SF_ = SF;
 }
 
 
 void MC33926::init()
 {
 	//Initializes the pins
-  pinMode(DIR1_,OUTPUT);
-  pinMode(DIR2_,OUTPUT);
-  pinMode(PWM_,OUTPUT);
-  pinMode(SF_,INPUT);
-  analogWriteFrequency(PWM_, 11718);
+	pinMode(DIR1_,OUTPUT);
+	pinMode(DIR2_,OUTPUT);
+	pinMode(PWM_,OUTPUT);
+	pinMode(SF_,INPUT);
+	#if defined(__MK20DX128__) || defined(__MK20DX256__)
+		analogWriteFrequency(PWM_, 11718);
+	#endif
 }
 
 
 void MC33926::set_pwm(int desired_pwm)
 {
-	// Sets the pwm value.  Will take negative pwm.
-  if (desired_pwm < 0)
-  {
-    digitalWrite(DIR1_,LOW);
-    digitalWrite(DIR2_,HIGH);
-	if (desired_pwm < -255)
-		desired_pwm = -255;
-	analogWrite(PWM_,-1*desired_pwm);
-  }
-  else
-  {
-    digitalWrite(DIR2_,LOW);
-    digitalWrite(DIR1_,HIGH);
-	if (desired_pwm > 255)
-		desired_pwm = 255;
-	analogWrite(PWM_,desired_pwm);
-
-  }
+	// Sets the pwm value.  Will take negative pwm values.
+	if (desired_pwm < 0)
+	{
+		digitalWrite(DIR1_,LOW);
+		digitalWrite(DIR2_,HIGH);
+		if (desired_pwm < -255)
+			desired_pwm = -255;
+		analogWrite(PWM_,-1*desired_pwm);
+	}
+	else
+  	{
+		digitalWrite(DIR2_,LOW);
+		digitalWrite(DIR1_,HIGH);
+		if (desired_pwm > 255)
+			desired_pwm = 255;
+		analogWrite(PWM_,desired_pwm);
+	}
 }
 
 bool MC33926::fault()
 {
 	// Checks if there is a fault
-  return !digitalRead(SF_); //Flip logic
+	return !digitalRead(SF_); //Flip logic
 }
--- PololuMC33926.h
+++ PololuMC33926.h
@@ -5,20 +5,20 @@
 
 class MC33926
 {
-  public: 
+	public: 
 	// Constructors
-    MC33926(int DIR1, int DIR2,int PWM, int SF); 
+	MC33926(int DIR1, int DIR2,int PWM, int SF); 
 	// Public functions
-    void init(); // Initializes the pins
-    void set_pwm(int desired_pwm);  //Sets the desired PWM value
-    bool fault();  // Checks for an fault and returns true if there is one
+	void init(); // Initializes the pins
+	void set_pwm(int desired_pwm);  //Sets the desired PWM value
+	bool fault();  // Checks for an fault and returns true if there is one
     
-  private:
+	private:
 	// Private variables
-    int DIR1_;
-    int DIR2_;
-    int PWM_;
-    int SF_;
+	int DIR1_;
+	int DIR2_;
+	int PWM_;
+	int SF_;
 };
 
 #endif
